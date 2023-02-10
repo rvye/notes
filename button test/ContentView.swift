3f481f9c5e2d83b7b46f586c891ddd96cb289d75
kay <a@a.com>
2023-02-09 21:18:30 -0600

    Initial Commit

---
+++ button test/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  button test
+//
+
+import SwiftUI
+import UIKit
+
+var note: String = ""
+var NoteFieldFocused: Bool = false
+
+// Round ButtonStyle
+struct WhiteButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding(.vertical, 6.0)
+            .padding(.horizontal, 180.0)
+            .clipShape(Capsule())
+            .foregroundColor(.blue)
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+                        
+            // test button
+            
+            // put @ bottom of screen, black on white text
+            Button("Test Button") {
+                print("pressed")
+            }
+            .padding(.bottom)
+            .buttonStyle(WhiteButton())
+            .frame(height: 36)
+            
+            
+            TextField("Note", text:$note).focused($NoteFieldFocused)
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ button test/button_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  button_testApp.swift
+//  button test
+//
+//  Created by kayla on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct button_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
