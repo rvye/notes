a117881afde0d256011d47f17a6b0ec9e49f1373
rye <rvyeinq@gmail.com>
2023-02-27 08:32:25 -0600

    Clean UI up alot

--- notes/notes/ContentView.swift
+++ notes/notes/ContentView.swift
@@ -3,7 +3,6 @@
 
 // UI Testing ONLY
 import SwiftUI
-import UIKit
 
 // Round ButtonStyle
 struct WhiteButton: ButtonStyle {
@@ -19,7 +18,6 @@ struct WhiteButton: ButtonStyle {
 struct ContentView: View {
     // Variables, used in TextField
     @State private var note: String = ""
-    @FocusState var NoteFieldFocused: Bool
 
     // Main view, displayed onscreen.
     var body: some View {
@@ -28,8 +26,26 @@ struct ContentView: View {
             }
             }
             .padding(.bottom)
-            .buttonStyle(WhiteButton())
+            .buttonStyle(PlainButtonStyle())
             .frame(height: 36)
+
+            Text("Note").font(.custom("Inter Regular", size: 24))
+            
+            // Back text
+            Text("Back").font(.custom("Inter Regular", size: 24))
+
+            // Edit text
+            Text("Edit").font(.custom("Inter Regular", size: 16)).foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+            
+            // Edit button
+            Circle()
+	        .fill(Color(#colorLiteral(red: 0.4749999940395355, green: 0.4749999940395355, blue: 0.4749999940395355, alpha: 1)))
+            .frame(width: 55, height: 55)
+
+            // Menu bar, bar
+            Rectangle()
+	        .fill(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
+            .frame(width: 430, height: 1)
         }
     }
 struct ContentView_Previews: PreviewProvider {
